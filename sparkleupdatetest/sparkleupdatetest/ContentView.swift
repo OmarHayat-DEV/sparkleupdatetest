3f55399bf8b1822fcf80c337d2afe7fb24982129
Omar Hayat <omar.hayat613@gmail.com>
2026-02-07 14:39:10 -0500

    make xcode project

---
+++ sparkleupdatetest/sparkleupdatetest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sparkleupdatetest
+//
+//  Created by Omar Hayat on 2026-02-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sparkleupdatetest/sparkleupdatetest/sparkleupdatetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkleupdatetestApp.swift
+//  sparkleupdatetest
+//
+//  Created by Omar Hayat on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct sparkleupdatetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
